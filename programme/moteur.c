6f1b94c86df93d46c76b4b1d6cf9c9d4bf765af0
glemmm <68287089+glemmm@users.noreply.github.com>
2020-12-10 09:06:22 +0100

    Update moteur.c
    Ajout d'un paramètre ancienne commande, qui permet d'éviter de relancer la même commande en boucle (vu qu'on l'utilise dans une boucle infinie).

--- programme/moteur.c
+++ programme/moteur.c
@@ -3,12 +3,25 @@
 #include <unistd.h>
 #include "moteur.h"
 
-void commande_robot(const char* nom_commande) {
+#define DEBUG 0 //commenter pour cacher printf
 
-	FILE* uart;
-	uart = fopen(UART_PERIPH, "w");
-	fprintf(uart, "%s\n",nom_commande);
-	fclose(uart);
+void commande_robot(const char* nom_commande, char* ancienne_commande) {
+	if(ancienne_commande == nom_commande)
+	{
+	#ifdef DEBUG  // debug avec la console
+		printf("commande déjà en cours");
+	#endif
+
+	}
+	else
+	 {
+	 	ancienne_commande = nom_commande; //Mis à jour de la nouvelle commande dans la variable qui mémorise
+	 	FILE* uart;
+		uart = fopen(UART_PERIPH, "w");
+		fprintf(uart, "%s\n",nom_commande);
+		fclose(uart);
+	 }
+
 
 }
 
