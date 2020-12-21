8472040787fcc73820515acd75a745c54a05fb48
glemmm <68287089+glemmm@users.noreply.github.com>
2020-12-21 11:48:12 +0100

    Séquence contournement + moyennage
    > Inclure stdio.h règle le problème de warning printf.
    > Séquence contournement améliorée. (Petit problème avec la commande "tg" qui doit faire pivoter le robot à une vitesse divisée par 2 mais il reste à vitesse maximale => il tourne plus vite que "td" qui pivote  à droite à la bonne vitesse.
    > Grand retour du Moyen-Âge (moyennage excellent) (ligne 116 -> 134).
    > J'ai testé chez moi : ça marche bien quand on règle bien le seuil de sum_tab. (à tester au préalable on se mettant à environ 15cm du capteur ultrason (ligne 75).

--- programme/robot.c
+++ programme/robot.c
@@ -1,62 +1,98 @@
 #include "moteur.h"
 #include "ultrason.h"
 #include "camera.h"
+#include<stdio.h> //éviter warning printf
 #define DEBUG 0 //commenter pour enlever tous les printf.
+
+void contourne_droite(unsigned char* ancienne_commande)
+{
+	commande_robot("st", ancienne_commande); // on recul un peu
+	usleep(1000000); //0.5sec
+	//On va essayer de le contourner par la droite 
+	commande_robot("av", ancienne_commande); // on recul un peu
+	usleep(1000000); //0.5sec
+	commande_robot("st", ancienne_commande); // on recul un peu
+	usleep(1000000); //0.5sec
+
+
+	commande_robot("td", ancienne_commande); // pivote à droite
+	usleep(2500000); //2sec
+	commande_robot("st", ancienne_commande); // on recul un peu
+	usleep(1000000); //0.5sec
+
+	commande_robot("ar", ancienne_commande); //on avance mais plus de détection d'obstacle, on prie pour que y'ait rien à sa droite
+	usleep(750000); //1sec
+	commande_robot("st", ancienne_commande); // on recul un peu
+	usleep(1000000); //0.5sec
+
+	commande_robot("tg", ancienne_commande); //pivote à gauche : soucis avec cette commande, la roue va à la vitesse max alors que j'ai /2 sur la teensy. Sur la commande pivote droite il n'y a pas ce soucis ...
+	usleep(1350000); //2sec
+	commande_robot("st", ancienne_commande); // on recul un peu
+	usleep(1000000); //0.5sec
+
+}
+
+
 main()
 {
 	unsigned char ancienne_commande[2]="st"; //stock la commande en cours pour ne pas relancer la même commande en boucle .. de base, st
+	
+	/*INITIALISATION CAMERA THERMIQUE*/	
 	int i2c1file;
 	i2c1_init(&i2c1file); //On passe le paramètre par référence.
-	int i2c2file;
-	i2c2_init(&i2c2file); //on passe par référénce
-
-
 	unsigned char I2C1_WR_Buf[MAX_BUFF_SIZE_WR]; // Buffer donnees envoyees
 	unsigned char I2C1_RD_Buf[MAX_BUFF_SIZE_RD]; // Buffer donnees recues
 	float temp_amb; // Température ambiante
 	float sum_tab; //Valeur qui stocke la somme de toutes les valeurs du tableau renvoyé par la caméra thermique
+	
+	/* initialisation ultrason */
+	unsigned char I2C2_WR_Buf[MAX_BUFF_SIZE];
+	unsigned char I2C2_RD_Buf[MAX_BUFF_SIZE];
+	int i2c2file;
+	i2c2_init(&i2c2file); //on passe par référénce
+	
+	/*INITIALISATION recherche source chaude*/
+	int cible; //indice de la cible = indice de la colonne du tableau la plus chaude
+	int cibles[9]; //tableau qui permet de moyenner : tab[x] correspond au nombre de fois qu'a été renvoyé l'indice x par la fonction qui chercher l'indice de la colonne la plus chaude
+	int max_cible; //permet de stocker temporairement le maximum du tableau ci-dessus
+	int i = 0;
 
 	/* Initialisation obstacle */
 	int obstacle = 0; //Au début, aucun obstacle devant
 
-	/* initialisation ultrason */
-	unsigned char I2C2_WR_Buf[MAX_BUFF_SIZE];
-	unsigned char I2C2_RD_Buf[MAX_BUFF_SIZE];
-
 	while(1)
 	{
-		/* initialisation CAMERA */
-		temp_amb = get_temp_amb(i2c1file, I2C1_RD_Buf, I2C1_WR_Buf); // Acquisition de la tempréature ambiante
-		get_image(i2c1file, I2C1_RD_Buf, I2C1_WR_Buf); //Les tableaux sont passés par référence pour qu'ils soient modifiés dans la fonction 
-		int cible = indice_chaleur(I2C1_RD_Buf,temp_amb, &sum_tab); 	/* retourne l'indice de la colonne la plus chaud */
-		//On passe sum tab par référence car c'est la fonction qui va écraser la valeur de cette variable. Elle permet de définir si on doit arrêter le robot ou contourner un obstacle.
-		#ifdef DEBUG
-			printf("cible : %i , température ambiante : %f \n", cible, temp_amb);
-		#endif
 
-		/* SEQUENCE CONTOURNEMENT OBJET */
+		
 		if(obstacle==1) //si la caméra détecte un objet chaud loin alors la somme est proche de celle d'une somme de température ambiante
 		{
-			if(sum_tab<6999)//Si la camera voit un objet mais loin (6999 a été determiné via des tests.
+			temp_amb = get_temp_amb(i2c1file, I2C1_RD_Buf, I2C1_WR_Buf); // Acquisition de la tempréature ambiante
+			get_image(i2c1file, I2C1_RD_Buf, I2C1_WR_Buf);
+			cible = indice_chaleur(I2C1_RD_Buf,temp_amb, &sum_tab);
+			#ifdef DEBUG
+				printf("somme tableau : %f \n", sum_tab);
+			#endif
+			if(sum_tab<6249)//Si la camera voit un objet mais loin (6249 a été determiné via des tests.
 			{
-				commande_robot("st", ancienne_commande); // on recul un peu
-				usleep(1000000); ///1sec
-				//On va essayer de le contourner par la droite 
-				commande_robot("av", ancienne_commande); // on recul un peu
-				usleep(500000); ///0.5sec
-				commande_robot("td", ancienne_commande); // pivote à droite
-				usleep(2000000); //2sec
-				commande_robot("ar", ancienne_commande); //on avance mais plus de détection d'obstacle, on prie pour que y'ait rien à sa droite
-				usleep(1000000); //1sec
-				commande_robot("tg", ancienne_commande); //pivote à gauche
-				usleep(2500000); //2sec
+				while(mesure_distance(i2c2file, I2C2_WR_Buf, I2C2_RD_Buf)<=15)
+				{
+					/* SEQUENCE CONTOURNEMENT OBJET */
+					contourne_droite(ancienne_commande);
+					/*FIN SEQUENCE*/
+					//On re mesure, si < 15 alors on a pas assez décalé vers la droite donc on recommence
+				}
 				obstacle = 0;
 			}
+			
 			else //si la camera détecte un objet chaud très près alors la somme est élevée
 			{
-				commande_robot("st", ancienne_commande); // on recul un peu
-				obstacle = 0;
-				usleep(2500000);
+				commande_robot("st", ancienne_commande); // on stop les moteurs
+				while(mesure_distance(i2c2file, I2C2_WR_Buf, I2C2_RD_Buf)<=15)
+				{
+					//Sans la boucle on prend le risque de partir dans la séquence pour contourner un objet
+					usleep(1500000); //1,5 seconde entre chaque tentative
+				}
+				obstacle = 0; //plus d'obstacle devant
 			}
 		}
 
@@ -69,7 +105,33 @@ main()
 		/*moteur : commande_robot(const char* nom_commande) avec nom de commande : "av", etc */
 
 		else
-		{
+		{	max_cible =0;
+			for(i=0;i<9;i=i+1)
+			{
+				cibles[i] =  0;
+
+			}
+			for(i=0;i<5;i=i+1)
+			{
+				usleep(30000);
+				temp_amb = get_temp_amb(i2c1file, I2C1_RD_Buf, I2C1_WR_Buf); // Acquisition de la tempréature ambiante
+				get_image(i2c1file, I2C1_RD_Buf, I2C1_WR_Buf);
+				cible = indice_chaleur(I2C1_RD_Buf,temp_amb, &sum_tab);
+				cibles[cible+1] =  cibles[cible+1] + 1;
+
+			}
+			for(i=0;i<9;i=i+1)
+			{
+				if(cibles[i] >= max_cible)
+				{
+					max_cible = cibles[i];
+					cible = i-1;
+				}
+
+			}
+			#ifdef DEBUG
+			printf("Cible après moyennage : %i \n", cible);
+			#endif
 			obstacle = 0; //pas d'obstacle
 			if(cible >=0) //On vérifie qu'une cible est présente. Via l'indice de la colonne la plus chaude.
 			{
