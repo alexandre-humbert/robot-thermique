a7840d173e06dc79d51b4363d015b5f5fb40f0a3
glemmm <68287089+glemmm@users.noreply.github.com>
2021-01-17 17:41:01 +0100

    Commentaires

--- programme/interface.c
+++ programme/interface.c
@@ -10,7 +10,7 @@ main(int argc, const char* argv[])
 	Moteur moteur;
 	
 	/*INITIALISATION CAMERA THERMIQUE*/	
-	camera_init(&camera,5);
+	camera_init(&camera, 3); //On moyenne sur 3 valeurs 
 	
 	/* initialisation ultrason */
 	ultrason_init(&ultrason,0x07,0x15);
@@ -18,10 +18,12 @@ main(int argc, const char* argv[])
 	/* INITIALISTION MOTEUR */
 	moteur_init(&moteur);
 	
-
+	/* Affichage distance, luminosité de la pièce, et indice de la source chaude (-1 si y'en a pas)) */
 	printf("<p>Distance : %i cm</p>",ultrason.distance);
 	printf("<p>Luminosité : %i</p>",ultrason.luminosite);
 	printf("<p>Source chaude en position : %i</p>",camera.indice);
+	
+	/* Affichage graphique de la matrice renvoyée par le capteur thermique */
 	camera_gen_image(&camera);
 
 
